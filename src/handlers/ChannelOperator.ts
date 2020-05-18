5bec09e18821a685073cdcab4535a1bda0b83981
Andrey Tkachenko <andre.tkachenko@outlook.com>
2020-05-18 18:28:02 +0300

    clear channel instead of recreating

--- src/handlers/ChannelOperator.ts
+++ src/handlers/ChannelOperator.ts
@@ -1,4 +1,4 @@
-import { VoiceState, TextChannel, GuildMember } from "discord.js";
+import { VoiceState, TextChannel, GuildMember, Collection, Message, VoiceChannel } from "discord.js";
 import { Dictionary } from "../collections/Dictionary";
 import { ChannelType } from "../enums/ChannelType"
 import { EnvType } from "../enums/EnvType";
@@ -37,7 +37,7 @@ export class ChannelOperator {
 
 			let voiceChannel = oldVoiceState.channel
 			if (voiceChannel?.members.size !== undefined && voiceChannel?.members.size <= 0) {
-				textChannel.messages.delete()
+				this.clearTextChannel(textChannel)
 			}
 		}
 	}
@@ -65,7 +65,8 @@ export class ChannelOperator {
 					},
 				]);
 				this.channelMap.Add(channelID, ch.id)
-				if(voiceChannel !== null) ch.send(`!${voiceChannel.name}`)
+				this.greet(ch, voiceChannel)
+				
 
 				if (process.env.NODE_ENV === EnvType.Debug) ch.send(`channel created for ${this.resolveUsername(user)}`); // test purposes only
 			});
@@ -83,12 +84,16 @@ export class ChannelOperator {
 		if (user != null) textChannel.updateOverwrite(user, { VIEW_CHANNEL: value })
 	}
 
-	private clearTextChannel(textChannel: TextChannel) {
-		let fetched;
+	private async clearTextChannel(textChannel: TextChannel) {
+		let fetched: Collection<string, Message>;
 		do {
-		  fetched = await textChannel.fetch({limit: 100});
+		  fetched = await textChannel.messages.fetch({limit: 100});
 		  textChannel.bulkDelete(fetched);
 		}
 		while(fetched.size >= 2);
+	}
+
+	private greet(textChannel: TextChannel, voiceChannel: VoiceChannel | null) {
+		if(voiceChannel !== null) textChannel.send(`!${voiceChannel.name}`)
 	}
 }
