4ad620d98aa1015d9199ebcbf31125f531221dd3
Andrey Tkachenko <andre.tkachenko@outlook.com>
2020-05-19 22:37:31 +0300

    fix help formatting

--- src/handlers/HelpHandlers.ts
+++ src/handlers/HelpHandlers.ts
@@ -21,8 +21,7 @@ export class HelpHandlers {
                 \`\`\`
                 !health - check if bot is up and running
                 !addintro - add info that should be shown in the linked text channel. write !help addintro to see details
-                !changeintro - replace info that should be shown in the linked text channel with the new parameters. write !help changeintro to see details                
-                \`\`\`
+                !changeintro - replace info that should be shown in the linked text channel with the new parameters. write !help changeintro to see details\`\`\`
                 `)
                 return
             }
@@ -40,34 +39,32 @@ export class HelpHandlers {
     private giveAddIntroHelp(message: Message) {
         message.channel.send(`
                 \`\`\`
-                !addintro - add info that should be shown in the linked text channel.\n
-                \n
-                Available fields:\n
-                ChannelName(required) - name of the voice channel for which this intro should be set\n
-                Description(optional) - any message (greetings, description, etc)\n
-                ImageUrl(optional) - link to the image/gif that should be sent after the mesage\n
-                AdditionalUrl(optional) - link to the message that should be sent after the mesage\n
-                \n                
-                Usage example:\n
-                !addintro { "ChannelName": "Default", "Description": "Welcome to the default channel of our server", "ImageUrl": "https://discord.com/assets/7edaed9d86e1b5dd9d4c98484372222b.svg", "AdditionalUrl": "https://discord.com/assets/d9b6a36b9077400c46cc64404100b59b.svg" }
-                \`\`\`
+                !addintro - add info that should be shown in the linked text channel.
+
+                Available fields:
+                ChannelName(required) - name of the voice channel for which this intro should be set
+                Description(optional) - any message (greetings, description, etc)
+                ImageUrl(optional) - link to the image/gif that should be sent after the mesage
+                AdditionalUrl(optional) - link to the message that should be sent after the mesage
+                             
+                Usage example:
+                !addintro { "ChannelName": "Default", "Description": "Welcome to the default channel of our server", "ImageUrl": "https://discord.com/assets/7edaed9d86e1b5dd9d4c98484372222b.svg", "AdditionalUrl": "https://discord.com/assets/d9b6a36b9077400c46cc64404100b59b.svg" }\`\`\`
                 `)
     }
 
     private giveChangeIntroHelp(message: Message) {
         message.channel.send(`
                 \`\`\`
-                !changeintro - replace info that should be shown in the linked text channel with the new parameters.\n
-                \n
-                Available fields:\n
-                ChannelName(required) - name of the voice channel for which this intro should be set\n
-                Description(optional) - any message (greetings, description, etc)\n
-                ImageUrl(optional) - link to the image/gif that should be sent after the mesage\n
-                AdditionalUrl(optional) - link to the message that should be sent after the mesage\n
-                \n                
-                Usage example:\n
-                !changeintro { "ChannelName": "Default", "Description": "Welcome to the default channel of our server", "ImageUrl": "https://discord.com/assets/7edaed9d86e1b5dd9d4c98484372222b.svg", "AdditionalUrl": "https://discord.com/assets/d9b6a36b9077400c46cc64404100b59b.svg" }
-                \`\`\`
+                !changeintro - replace info that should be shown in the linked text channel with the new parameters.
+                
+                Available fields:
+                ChannelName(required) - name of the voice channel for which this intro should be set
+                Description(optional) - any message (greetings, description, etc)
+                ImageUrl(optional) - link to the image/gif that should be sent after the mesage
+                AdditionalUrl(optional) - link to the message that should be sent after the mesage
+                                
+                Usage example:
+                !changeintro { "ChannelName": "Default", "Description": "Welcome to the default channel of our server", "ImageUrl": "https://discord.com/assets/7edaed9d86e1b5dd9d4c98484372222b.svg", "AdditionalUrl": "https://discord.com/assets/d9b6a36b9077400c46cc64404100b59b.svg" }\`\`\`
                 `)        
     }
 }
