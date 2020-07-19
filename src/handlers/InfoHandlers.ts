8ca276350642ebdaf2b5756918ab8cbbe8015edb
Andrey Tkachenko <andre.tkachenko@outlook.com>
2020-07-19 12:50:13 +0300

    add how-to-use field in help cmd

--- src/handlers/InfoHandlers.ts
+++ src/handlers/InfoHandlers.ts
@@ -19,10 +19,14 @@ export class InfoHandlers {
     private giveHelp(message: Message) {
         let embed = new MessageEmbed()
             .setTitle("Nexus Bot")
-            .setDescription("Discord bot to link text channel to each voice channel.")
+            .setDescription(`Discord bot to link text channel to each voice channel.`)
             .setColor("#0099ff")
             .setAuthor('Nexus', this.config.img, 'https://github.com/andretkachenko/nexus-bot')
             .setThumbnail(this.config.img)
+            .addField("**How to use**",
+            `You don't' need to set up anything - once you join a voice channel, a new category with the linked text channel will be created.
+            Each time user joins/leaves voice channel, he will get/lose rights to see the linked text channel.
+            Feel free to rename categories and text channels as you wish - it will not affect bot.`)
             .addField("**Want to use it on your server?**", "Follow this link: https://github.com/andretkachenko/nexus-bot#want-to-use-at-your-server")
             .addField("**Any issues or missing feature?**", "You can suggest it via https://github.com/andretkachenko/nexus-bot/issues")
             .setFooter(`Nexus bot`);
