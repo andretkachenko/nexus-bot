6a5fd6539ec8eeefd1e3a716a99f9636a995b9ea
Andrey Tkachenko <andre.tkachenko@outlook.com>
2021-05-27 10:48:42 +0300

    fix documentation for preserve
    Resolves #144

--- src/handlers/userCommands/Preserve.ts
+++ src/handlers/userCommands/Preserve.ts
@@ -48,12 +48,14 @@ export class Preserve extends BaseHandler {
 		embed
 			.addField(`${this.cmd} [0/1] {channelId}`, `
 			Enable/disable clearance of the Text Channel after the last User has left associated Voice Channel.
-            \`1\` means that messages should not be deleted once the Voice Channel is empty, \`0\` - delete all unpinned messages after the last User has left the Voice Channel (0 is the default option for the each Text Channel the bot creates).
+            \`1\` means that messages should not be deleted once the Voice Channel is empty, \`0\` - delete all unpinned messages after the last User has left the Voice Channel (0 is the default option for each Text Channel the bot creates).
             The pinned messages will remain in all Nexus-handled Text Channels with both options.
+			Supports arguments chaining - you're allowed to use more than 1 Voice Channel ID.
+			Note: You need to use Voice Channel ID, not mapped Text Channel ID.
 
             Examples:
-            \`${this.cmd} 1 717824008636334130\` - request to keep message in the Text Channel with the ID \`717824008636334130\`
-            \`${this.cmd} 0 717824008636334130\` - request to resume message deletion in the Text Channel with the ID \`717824008636334130\`
+            \`${this.cmd} 1 717824008636334130\` - request to keep message in the Text Channel that is mapped to a Voice Channel with the ID \`717824008636334130\`
+            \`${this.cmd} 0 717824008636334130\` - request to resume message deletion in the Text Channel that is mapped to a Voice Channel with the ID \`717824008636334130\`
             
             Requires user to have admin/owner rights or permissions to manage channels and roles.
 		`)
