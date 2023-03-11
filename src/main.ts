64d8e3b0357536d5cc0eb923fa983b8482cd1917
Andrey Tkachenko <andre.tkachenko@outlook.com>
2023-03-11 13:50:20 +0200

    add health check

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,25 @@
 'use strict'
 
 import { Bot } from './Bot'
+import express from 'express'
+import http from 'http'
 
 const client: Bot = new Bot()
 client.start()
+
+const app = express()
+const router = express.Router()
+
+router.use((req, res, next) => {
+	res.header('Access-Control-Allow-Methods', 'GET')
+	next()
+})
+
+router.get('/health', (req, res) => {
+	res.status(200).send('Ok')
+})
+
+app.use('/', router)
+
+const server = http.createServer(app)
+server.listen(3000)
